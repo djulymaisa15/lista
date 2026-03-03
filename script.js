bd5c70b1c6f00f946654796c5ad1369b103e6505
djulymaisa15 <silva.djuly@escola.pr.gov.br>
2026-03-03 16:26:24 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,35 @@
-// Lógica que o botão executa por trás
-for (String pet : listaDePets) {
-    if (pet.toUpperCase().contains("A")) {
-        System.out.println(pet.toUpperCase() + " possui a letra A!");
+// A Lista de convidados pets
+const pets = ["Jorge", "Joaquim", "Mikaelly", "Laura", "Nicole", "Thamily"];
+const listaUl = document.getElementById('lista-pets');
+
+// Lógica de filtragem: Apenas nomes > 5 letras
+// Simulando a função RANGE percorrendo o array
+for (let i = 0; i < pets.length; i++) {
+    let nomePet = pets[i];
+    
+    if (nomePet.length > 5) {
+        let li = document.createElement('li');
+        li.className = 'pet-item';
+        li.textContent = nomePet;
+        listaUl.appendChild(li);
     }
 }
+
+// Fazendo o botão funcionar
+document.getElementById('btn-verificar').onclick = function() {
+    const itensVisiveis = document.querySelectorAll('.pet-item');
+    let contador = 0;
+
+    itensVisiveis.forEach(pet => {
+        // Verifica se o nome possui a letra "A"
+        if (pet.textContent.toLowerCase().includes('a')) {
+            pet.classList.add('brilho'); // Ativa o CSS de brilho
+            contador++;
+        }
+    });
+
+    // Mostra o placar com o total
+    const infoPlacar = document.getElementById('placar');
+    document.getElementById('valor').textContent = contador;
+    infoPlacar.classList.remove('hidden');
+};
