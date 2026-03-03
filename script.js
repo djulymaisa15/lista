56570b2f7d7a5d9985df3c19aa3ffa656ecc1c7b
djulymaisa15 <silva.djuly@escola.pr.gov.br>
2026-03-03 16:19:04 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.IntStream;
 
-public class PetShopSistema {
+public class PetShopAnalise {
     public static void main(String[] args) {
         List<String> pets = Arrays.asList("Jorge", "Joaquim", "Mikaelly", "Laura", "Nicole", "Thamily");
         
-        // Simulação do processamento de nomes com mais de 5 letras usando Range
-        IntStream.range(0, pets.size())
-                 .filter(i -> pets.get(i).length() > 5)
-                 .forEach(i -> System.out.println("PET FILTRADO: " + pets.get(i).toUpperCase()));
+        long totalComA = pets.stream()
+            .filter(nome -> nome.toUpperCase().contains("A"))
+            .count();
+
+        System.out.println("Nomes que possuem a letra A: " + totalA);
     }
 }
