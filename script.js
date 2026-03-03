1338140411daa0ba5ba005ec3efc3f5168bacb46
djulymaisa15 <silva.djuly@escola.pr.gov.br>
2026-03-03 16:22:43 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
-import java.util.Arrays;
-import java.util.List;
-
-public class PetShopAnalise {
-    public static void main(String[] args) {
-        List<String> pets = Arrays.asList("Jorge", "Joaquim", "Mikaelly", "Laura", "Nicole", "Thamily");
-        
-        long totalComA = pets.stream()
-            .filter(nome -> nome.toUpperCase().contains("A"))
-            .count();
-
-        System.out.println("Nomes que possuem a letra A: " + totalA);
+// Lógica que o botão executa por trás
+for (String pet : listaDePets) {
+    if (pet.toUpperCase().contains("A")) {
+        System.out.println(pet.toUpperCase() + " possui a letra A!");
     }
 }
